834c1ffea4fbb65ae32daafef356cf90bfc99d6b
Mhassan0001 <hassanfareed5050@gmail.com>
2025-08-29 22:54:42 +0500

    wip: add validateRequest middleware (incomplete)

---
+++ Src/Middlewares/validateRequest.js
@@ -0,0 +1,10 @@
+const { validationResult } = require("express-validator");
+
+let validateRequest = (req, res, next) => {
+    let error = validateRequest(req);
+    if (!error.isempty()) {
+        
+    }
+};
+
+module.exports = {};
