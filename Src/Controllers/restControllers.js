965bb1233b34a89e7d37c80384692e1008f62bff
Mhassan0001 <hassanfareed5050@gmail.com>
2025-09-12 22:30:26 +0500

    feat: add dynamic sorting to read route

--- Src/Controllers/restControllers.js
+++ Src/Controllers/restControllers.js
@@ -1,5 +1,4 @@
 let collection = require("../Model/restModel");
-
 // *============================================================
 
 let check = (req, res) => {
@@ -22,25 +21,37 @@ let create = async (req, res) => {
 
 let read = async (req, res) => {
   try {
-    //Todo - Creating & Working On Sorting-feature Branch
     let userId = req.user.userId;
+    let data;
+    //  ?============================================
+    //! Pagination-Query's
     let page = parseInt(req.query.page) || 1;
     let limit = parseInt(req.query.limit) || 10;
     let skip = (page - 1) * limit;
-    //  ?============================================
-    let data;
     let totalItems;
     let totalPages;
+    //  ?============================================
+    //! Sorting-Query's
+    let sortField = req.query.sortField || "createdBy";
+    let sortOrder = req.query.sortOrder === "asc" ? 1 : -1;
     //  ?============================================
     if (req.user.role === "admin") {
       totalItems = await collection.countDocuments();
       totalPages = Math.ceil(totalItems / limit);
-      data = await collection.find().skip(skip).limit(limit);
+      data = await collection
+        .find()
+        .sort({ [sortField]: sortOrder })
+        .skip(skip)
+        .limit(limit);
       res.status(200).json({ data, page, limit, totalItems, totalPages });
     } else {
       totalItems = await collection.countDocuments({ userId });
       totalPages = Math.ceil(totalItems / limit);
-      data = await collection.find({ userId }).skip(skip).limit(limit);
+      data = await collection
+        .find({ userId })
+        .sort({ [sortField]: sortOrder })
+        .skip(skip)
+        .limit(limit);
       res.status(200).json({ data, page, limit, totalItems, totalPages });
     }
   } catch (error) {
