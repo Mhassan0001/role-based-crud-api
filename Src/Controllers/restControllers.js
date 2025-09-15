96abe328d42756e2a2555ccd6a8de1719c49c856
Mhassan0001 <hassanfareed5050@gmail.com>
2025-09-15 23:57:20 +0500

    add filtering in read API

--- Src/Controllers/restControllers.js
+++ Src/Controllers/restControllers.js
@@ -35,20 +35,27 @@ let read = async (req, res) => {
     let sortField = req.query.sortField || "createdBy";
     let sortOrder = req.query.sortOrder === "asc" ? 1 : -1;
     //  ?============================================
+    //! Filtering-Query's
+    let filter = {};
+    if (req.query.title) filter.title = req.query.title;
+    if (req.query.content) filter.content = req.query.content;
+    if (req.query.createdBy) filter.createdBy = req.query.createdBy;
+    //  ?============================================
     if (req.user.role === "admin") {
       totalItems = await collection.countDocuments();
       totalPages = Math.ceil(totalItems / limit);
       data = await collection
-        .find()
+        .find(filter)
         .sort({ [sortField]: sortOrder })
         .skip(skip)
         .limit(limit);
       res.status(200).json({ data, page, limit, totalItems, totalPages });
     } else {
-      totalItems = await collection.countDocuments({ userId });
+      filter.userId = userId;
+      totalItems = await collection.countDocuments(filter);
       totalPages = Math.ceil(totalItems / limit);
       data = await collection
-        .find({ userId })
+        .find(filter)
         .sort({ [sortField]: sortOrder })
         .skip(skip)
         .limit(limit);
